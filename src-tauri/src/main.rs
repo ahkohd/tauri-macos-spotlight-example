422e0f14b111ecc0c1c651e8fc7eb40709de6c4b
Victor Aremu <victor.olorunbunmi@gmail.com>
2023-03-01 07:16:42 +0100

    feat: Tauri NSPanel spotlight app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,13 +9,12 @@ fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             spotlight::init_spotlight_window,
+            spotlight::show_spotlight,
             spotlight::hide_spotlight
         ])
         .manage(spotlight::State::default())
         .setup(move |app| {
-            // Set the app's activation poicy to Accessory does the following behaviours:
-            // - Makes the windows of this app appear above full-screen windows of other apps.
-            // - Prevents the app's icon from showing on the dock.
+            // Set activation poicy to Accessory to prevent the app icon from showing on the dock
             app.set_activation_policy(tauri::ActivationPolicy::Accessory);
             Ok(())
         })
